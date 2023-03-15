6afc182b2db00bd04c88895355361e45208b6950
Eric Drake <eric.drake2011@gmail.com>
2023-03-15 09:12:58 -0500

    moved common fields to AbstractEntity for extension and extended

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,6 +1,28 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @NotBlank
+    @Size(max = 255)
+    private String location;
+
+    public Employer(String aLocation) {
+        location = aLocation;
+    }
+
+    public Employer() {}
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String aLocation) {
+        location = aLocation;
+    }
 
 }
